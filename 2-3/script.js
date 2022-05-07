76cbfbf62de9b3915caa5ab91f70fd8005c53c13
Renato Knauer <knauer.rm@gmail.com>
2022-05-06 23:56:54 -0300

    challenge 2-3; minimize button added

--- 2-3/script.js
+++ 2-3/script.js
@@ -1,6 +1,8 @@
 const modalOverlay = document.querySelector('.modal-overlay')
 const cards = document.querySelectorAll('.card')
 const modal = document.querySelector('.modal')
+const maximizeModal = document.querySelector('.maximize-modal')
+const minimizeModal = document.querySelector('.minimize-modal')
 
 for (let card of cards) {
     card.addEventListener('click', function(){
@@ -8,6 +10,7 @@ for (let card of cards) {
 
         modalOverlay.classList.add('active')
         modalOverlay.querySelector('iframe').src = `https://blog.rocketseat.com.br/${postId}`
+        document.querySelector('footer').style.visibility = 'hidden'
     })
 }
 
@@ -18,6 +21,9 @@ document.querySelector('.close-modal').addEventListener('click', function(){
     modal.style.height = '90%'
     modal.style.width = '90%'
     modal.style.margin = '5vh auto'
+    maximizeModal.style.visibility = 'visible'
+    minimizeModal.style.visibility = 'hidden'
+    document.querySelector('footer').style.visibility = 'visible'
 })
 
 document.querySelector('.maximize-modal').addEventListener('click', function(){
@@ -25,4 +31,15 @@ document.querySelector('.maximize-modal').addEventListener('click', function(){
     modal.style.height = '100%'
     modal.style.width = '100%'
     modal.style.margin = '0'
+    maximizeModal.style.visibility = 'hidden'
+    minimizeModal.style.visibility = 'visible'
+})
+
+document.querySelector('.minimize-modal').addEventListener('click', function(){
+    modal.style.position = 'relative'
+    modal.style.height = '90%'
+    modal.style.width = '90%'
+    modal.style.margin = '5vh auto'
+    maximizeModal.style.visibility = 'visible'
+    minimizeModal.style.visibility = 'hidden'
 })
