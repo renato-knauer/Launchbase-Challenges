32e17a5bfbb9df2fb457178ef52a9448a088d0b3
Renato Knauer <knauer.rm@gmail.com>
2022-05-07 00:31:11 -0300

    challenge 2-3

--- 2-3/script.js
+++ 2-3/script.js
@@ -4,7 +4,25 @@ const modal = document.querySelector('.modal')
 const maximizeModal = document.querySelector('.maximize-modal')
 const minimizeModal = document.querySelector('.minimize-modal')
 
+function returnModalToStandard(modal, maximizeModal, minimizeModal) {
+    modal.style.position = 'relative'
+    modal.style.height = '90%'
+    modal.style.width = '90%'
+    modal.style.margin = '5vh auto'
+    maximizeModal.style.visibility = 'visible'
+    minimizeModal.style.visibility = 'hidden'
+}
+
 for (let card of cards) {
+    card.addEventListener('mouseover', function(){
+        card.style.cursor = 'pointer'
+        card.style.opacity = '0.5'
+    })
+    
+    card.addEventListener('mouseout', function(){
+        card.style.opacity = '1'
+    })
+    
     card.addEventListener('click', function(){
         const postId = card.getAttribute('id')
 
@@ -17,13 +35,9 @@ for (let card of cards) {
 document.querySelector('.close-modal').addEventListener('click', function(){
     modalOverlay.classList.remove('active')
     modalOverlay.querySelector('iframe').src = ''
-    modal.style.position = 'relative'
-    modal.style.height = '90%'
-    modal.style.width = '90%'
-    modal.style.margin = '5vh auto'
-    maximizeModal.style.visibility = 'visible'
-    minimizeModal.style.visibility = 'hidden'
     document.querySelector('footer').style.visibility = 'visible'
+
+    returnModalToStandard(modal, maximizeModal, minimizeModal)
 })
 
 document.querySelector('.maximize-modal').addEventListener('click', function(){
@@ -36,10 +50,5 @@ document.querySelector('.maximize-modal').addEventListener('click', function(){
 })
 
 document.querySelector('.minimize-modal').addEventListener('click', function(){
-    modal.style.position = 'relative'
-    modal.style.height = '90%'
-    modal.style.width = '90%'
-    modal.style.margin = '5vh auto'
-    maximizeModal.style.visibility = 'visible'
-    minimizeModal.style.visibility = 'hidden'
+    returnModalToStandard(modal, maximizeModal, minimizeModal)
 })
