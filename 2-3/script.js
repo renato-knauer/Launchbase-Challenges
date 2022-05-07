5afa32a642f512012fda7d45d8f6d2ad7322612e
Renato Knauer <knauer.rm@gmail.com>
2022-05-06 23:37:24 -0300

    challenge 2-3; modal and maximize button added

--- 2-3/script.js
+++ 2-3/script.js
@@ -1,17 +1,28 @@
 const modalOverlay = document.querySelector('.modal-overlay')
 const cards = document.querySelectorAll('.card')
+const modal = document.querySelector('.modal')
 
 for (let card of cards) {
     card.addEventListener('click', function(){
-        const videoId = card.getAttribute('id')
+        const postId = card.getAttribute('id')
 
         modalOverlay.classList.add('active')
-        modalOverlay.querySelector('iframe').src = `https://www.youtube.com/embed/${videoId}`
+        modalOverlay.querySelector('iframe').src = `https://blog.rocketseat.com.br/${postId}`
     })
 }
 
 document.querySelector('.close-modal').addEventListener('click', function(){
     modalOverlay.classList.remove('active')
     modalOverlay.querySelector('iframe').src = ''
+    modal.style.position = 'relative'
+    modal.style.height = '90%'
+    modal.style.width = '90%'
+    modal.style.margin = '5vh auto'
 })
 
+document.querySelector('.maximize-modal').addEventListener('click', function(){
+    modal.style.position = 'absolute'
+    modal.style.height = '100%'
+    modal.style.width = '100%'
+    modal.style.margin = '0'
+})
